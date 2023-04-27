bf9e2aee691c542af4e31023bfab17934269dffc
Harrison Fisher <HarrisonFisher2018@gmail.com>
2023-04-26 20:12:12 -0700

    Update query_harrison.sql

--- query_harrison.sql
+++ query_harrison.sql
@@ -1,1 +1,6 @@
 
+
+-- What flights are scheduled to depart Houston/Intercontinental today
+SELECT *
+FROM Project1.flight_info
+WHERE Arrival_Departure = TRUE AND To_From = 'Houston/Intercontinental';
